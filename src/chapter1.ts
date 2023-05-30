108daeb9060d46dc236b557e4964bf5f004172a4
Stilllee <orodae@gmail.com>
2023-05-30 17:48:25 +0900

    5.1 인터페이스

--- src/chapter1.ts
+++ src/chapter1.ts
@@ -1,39 +1,24 @@
 /**
- * 함수 타입 정의
+ * 인터페이스
  */
 
-// 함수를 설명하는 가장 좋은 방법
-// 어떤 매개변수를 받고, 어떤 결과값을 반환하는지 이야기
-//어떤 [타입의] 매개벼수를 받고, 어떤 [타입의] 결과값을 반환하는지 이야기
-function func(a: number, b: number) {
-  return a + b;
+interface Person {
+  readonly name: string;
+  age?: number;
+  sayHi(): void;
+  sayHi(a: number, b: number): void;
 }
 
-/**
- * 화살표 함수의 타입을 정의하는 방법
- */
+type Type1 = number | string;
+type Type2 = number & string;
 
-const add = (a: number, b: number) => a + b;
+const person: Person | number = {
+  name: "이정환",
+  age: 27,
+  sayHi: function () {
+    console.log("Hi");
+  },
+};
 
-/**
- * 함수의 매개변수
- */
-
-function introduce(name = "이정환", age: number, tall?: number) {
-  console.log(`name : ${name}`);
-  if (typeof tall === "number") {
-    console.log(`tall : ${tall + 10}`);
-  }
-}
-
-introduce("이정환", 27, 175);
-
-introduce("이정환", 27);
-
-function getSum(...rest: [number, number, number]) {
-  let sum = 0;
-  rest.forEach((it) => (sum += it));
-}
-
-getSum(1, 2, 3); // 6
-// getSum(1, 2, 3, 4, 5); // 15
+person.sayHi();
+person.sayHi(1, 2);
